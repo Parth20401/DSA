2ed36ab53c12a3ac48487a0a36e618a3a49b7447
Parth Dayal <97301303+Parth20401@users.noreply.github.com>
2023-04-01 03:20:41 +0530

    Create Fruit Into Baskets.cpp

---
+++ Sliding Window & two pointer/Fruit Into Baskets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int,int>mp;
+        int i=0,j=0;
+        int ans=0;
+        while(j<fruits.size()){
+            mp[fruits[j]]++; //assign count of all fruits
+
+            if(mp.size()>2){
+                if(mp[fruits[i]]==1)
+                    mp.erase(fruits[i]);
+                else
+                    mp[fruits[i]]--;
+                
+                i++;
+            }
+            ans=max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+};
