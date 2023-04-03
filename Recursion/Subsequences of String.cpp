6fbbf7950e4b1379dfa8e74cb85004a8ac8880fd
Parth Dayal <97301303+Parth20401@users.noreply.github.com>
2023-04-04 01:25:36 +0530

    Create Subsequences of String.cpp

---
+++ Recursion/Subsequences of String.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+
+void solve(string str,string output,int index,vector<string>& ans){
+	//base case
+	if(index >= str.length()){
+		if(output.length()>0){
+			ans.push_back(output);
+		}
+		return;
+	}
+
+	//exclusion
+	solve(str,output,index+1,ans);
+
+	//inclusion
+	char element = str[index];
+	output.push_back(element);
+	solve(str,output,index+1,ans);
+}
+vector<string> subsequences(string str){
+	
+	vector<string> ans;
+	string output="";
+	int index=0;
+	solve(str,output,index,ans);
+	return ans;	
+}
