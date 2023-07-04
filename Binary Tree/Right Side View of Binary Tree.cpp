17a5fac382cb00e76132cfc044083fabce60f89d
Parth Dayal <97301303+Parth20401@users.noreply.github.com>
2023-07-05 00:12:15 +0530

    Create Right Side View of Binary Tree.cpp

---
+++ Binary Tree/Right Side View of Binary Tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rightView(TreeNode* root, int level, vector<int>& ans){
+        if(root == NULL)
+            return;
+
+        if(level == ans.size())
+            ans.push_back(root->val);
+
+        rightView(root->right, level + 1, ans);
+        rightView(root->left, level + 1, ans);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        rightView(root, 0, ans);
+        return ans;
+    }
+};
