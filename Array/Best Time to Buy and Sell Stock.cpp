a01edc5226f7cb29d789f11586ff44e85d3d0396
Parth Dayal <97301303+Parth20401@users.noreply.github.com>
2023-07-18 01:50:33 +0530

    Update Best Time to Buy and Sell Stock.cpp

--- Array/Best Time to Buy and Sell Stock.cpp
+++ Array/Best Time to Buy and Sell Stock.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        if(n < 2) return 0;
-
-        int min_price = prices[0];
+        int mini = prices[0]; //assume first element to be min
         int max_profit = 0;
 
-        for(int i=1; i<n ;i++){
-            max_profit = max(max_profit, prices[i] - min_price);
-            min_price = min(min_price, prices[i]);
+        for(int i=1; i<n; i++){
+            int cost = prices[i] - mini; //the cost is what we get after (SP - CP) 
+            max_profit = max(max_profit, cost);//maximinzing profit
+            mini = min(mini, prices[i]); //update mini to get min price of buying
         }
+
         return max_profit;
     }
 };
