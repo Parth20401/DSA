dac0afdc8ba9ad3787d745fe13f47fdf954ea29c
Parth Dayal <97301303+Parth20401@users.noreply.github.com>
2024-01-18 01:18:37 +0530

    Update Generate all binary strings.cpp

--- Recursion/Generate all binary strings.cpp
+++ Recursion/Generate all binary strings.cpp
@@ -1,47 +1,23 @@
-//{ Driver Code Starts
-//Initial Template for C++
-#include<bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function Template for C++
-
-class Solution{
-public:
-    void find(int num,string ans){
-        if(num==0){
-            cout<<ans<<" ";
-            return;
+void solve(int N, vector<string>& ans, string output, int idx){
+    //base case
+    if (idx == N) {
+      ans.push_back(output);
+      return;
     }
-    find(num-1,ans+'0');
-    
-    if(ans.size()==0 || ans.back()!='1')
-        find(num-1,ans+'1');
+
+    //adding 0
+    solve(N, ans, output + '0', idx + 1);
+
+    //adding 1
+    if((idx == 0) || (output[idx - 1] != '1'))
+        solve(N, ans, output + '1', idx + 1);
+        
 }
 
-    void generateBinaryStrings(int& num){
-        find(num,"");
-        return;
-    }
-};
-
-//{ Driver Code Starts.
-int main(){
-    int t = 1;
-    cin >> t;
-
-
-    while(t--){
-        //Input
-        int n; cin >> n;
-
-        Solution obj;
-        obj.generateBinaryStrings(n);
-        cout << endl;
-    }
-
-    return 0;
+vector<string> generateString(int N) {
+    vector<string> ans;
+    solve(N, ans, "", 0);
+    return ans;
 }
-
-// } Driver Code Ends
+//Tc - O(2^N)
+//SC - O(N)
