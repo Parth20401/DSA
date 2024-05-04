ba0697d02618aa1d8ae0128719243e549c67a440
Parth Dayal <97301303+Parth20401@users.noreply.github.com>
2024-05-05 00:00:32 +0530

    Update Insertion sort.cpp

--- Sorting/Insertion sort.cpp
+++ Sorting/Insertion sort.cpp
@@ -1,19 +1,20 @@
-class Solution
-{
-    public:
-    //Function to sort the array using bubble sort algorithm.
-    void bubbleSort(int arr[], int n)
-    {
-        //move from the last element and keep the sorted element in there
-        for(int i=n-1; i>=0; i--){
-            //now j loop will go through entire array and compare the adjacent element
-            //until i-1 goes(bcos after that all elements are already sorted)
-            for(int j=0; j<i; j++){
-                if(arr[j] > arr[j + 1])
-                    swap(arr[j], arr[j + 1]);
-            }
+void insertion_sort(int arr[], int n) {
+    for (int i = 0; i <= n - 1; i++) {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
         }
     }
-};
+
+    cout << "After Using insertion sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
 //TC - O(n^2)-worst and average
 //O(n)- best case
